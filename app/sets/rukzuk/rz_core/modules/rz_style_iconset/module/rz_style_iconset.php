94054f72827398d315494b5460a7e3cce9d88a16
Sebastian Roller <sebastian.roller@rukzuk.com>
2016-05-09 14:37:36 +0200

    rz_style_iconset: first version of icon set module

---
+++ app/sets/rukzuk/rz_core/modules/rz_style_iconset/module/rz_style_iconset.php
@@ -0,0 +1,18 @@
+<?php
+namespace Rukzuk\Modules;
+
+class rz_style_iconset extends SimpleModule {
+
+  /**
+   * Output content for HTML head
+   * @param \Render\APIs\APIv1\CSSAPI $api
+   * @param \Render\Unit $unit
+   * @param \Render\ModuleInfo $moduleInfo
+   * @return string
+   */
+  public function htmlHeadUnit($api, $unit, $moduleInfo)
+  {
+	$html = '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">';
+    return $html;
+  }
+}
