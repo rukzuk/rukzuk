63ea1f653850c568c525c7cb356621cadc322623
Sebastian Roller <sebastian.roller@rukzuk.com>
2016-06-17 15:18:01 +0200

    rz_selector_pseudo_class: Event states integrated

--- app/sets/rukzuk/rz_core/modules/rz_selector_pseudo_class/module/rz_selector_pseudo_class.php
+++ app/sets/rukzuk/rz_core/modules/rz_selector_pseudo_class/module/rz_selector_pseudo_class.php
@@ -1,4 +1,20 @@
 <?php
 namespace Rukzuk\Modules;
 
-class rz_selector_pseudo_class extends SimpleModule { }
+class rz_selector_pseudo_class extends SimpleModule {
+
+  public function htmlHeadUnit($api, $unit, $moduleInfo)
+  {
+    $parentUnit = $api->getParentUnit($unit);
+    $selector = '#'.$parentUnit->getId();
+    $stateName = $api->getFormValue($unit, 'pseudoClass');
+
+
+    // enable animation only if this extension unit is a direct child of default unit
+    if ((!$api->getModuleInfo($parentUnit)->isExtension()) && ($stateName != "")) {
+      $code = '<script>$(function() { $("' . $selector . '").addClass("listen_' . $stateName . '")});</script>';
+    }
+    return $code;
+  }
+
+}
