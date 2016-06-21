030c780e798dd57feb951141a204d6b10a6d8ae0
Sebastian Roller <sebastian.roller@rukzuk.com>
2016-06-21 12:24:54 +0200

    rz_selector_pseudo_class: bugfix

--- app/sets/rukzuk/rz_core/modules/rz_selector_pseudo_class/module/rz_selector_pseudo_class.php
+++ app/sets/rukzuk/rz_core/modules/rz_selector_pseudo_class/module/rz_selector_pseudo_class.php
@@ -13,8 +13,8 @@ class rz_selector_pseudo_class extends SimpleModule {
     // enable animation only if this extension unit is a direct child of default unit
     if ((!$api->getModuleInfo($parentUnit)->isExtension()) && ($stateName != "")) {
       $code = '<script>$(function() { $("' . $selector . '").addClass("listen_' . $stateName . '")});</script>';
+      return $code;
     }
-    return $code;
   }
 
 }
