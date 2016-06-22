295a788d6e18ceed1e13225d751c825cbb5fa473
Sebastian Roller <sebastian.roller@rukzuk.com>
2016-06-22 16:28:48 +0200

    rz_trigger_event: code optimization

--- app/sets/rukzuk/rz_core/modules/rz_trigger_event/assets/notlive/setModuleName.js
+++ app/sets/rukzuk/rz_core/modules/rz_trigger_event/assets/notlive/setModuleName.js
@@ -1,12 +1,12 @@
-define(['rz_root/notlive/js/baseJsModule', 'CMS', 'jquery'], function (JsModule, CMS, $) {
+define(['rz_root/notlive/js/baseJsModule', 'CMS', 'jquery'], function (JsModule, CMS) {
 
     var updateName = function (cfg) {
         var moduleData = CMS.get(cfg.unitId, true);
 
-        var moduleName = moduleData.formValues['eventType'].value;
+        var moduleName = moduleData.formValues.eventType.value;
         moduleName = moduleName.charAt(0).toUpperCase() + moduleName.substr(1);
-        if (moduleData.formValues['enableState'].value) {
-            moduleName += ' - ' + moduleData.formValues['stateName'].value;
+        if (moduleData.formValues.enableState.value) {
+            moduleName += ' - ' + moduleData.formValues.stateName.value;
         }
         CMS.setInfo(cfg.unitId, 'name', moduleName);
     };
@@ -27,5 +27,5 @@ define(['rz_root/notlive/js/baseJsModule', 'CMS', 'jquery'], function (JsModule,
 
             });
         }
-    }
+    };
 });
