b4f210f050b6ef22073ad0fef9b3741ab3b4fac4
Sebastian Roller <sebastian.roller@rukzuk.com>
2016-08-18 16:36:42 +0200

    code optimization

--- app/sets/rukzuk/rz_core/modules/rz_style_styleset/assets/notlive/styleset.js
+++ app/sets/rukzuk/rz_core/modules/rz_style_styleset/assets/notlive/styleset.js
@@ -11,15 +11,15 @@ define(['jquery', 'CMS', 'rz_root/notlive/js/cssHelper'], function ($, CMS, cssH
                 delete unitData.parentUnitId;
             }
             delete unitData.children;
-            if (unitData.formValues.cssStyleSetName.value != '') {
+            if (unitData.formValues.cssStyleSetName.value !== '') {
                 allCssClassModules.push(unitData);
             }
 
         });
         allCssClassModules.forEach(function (node) {
-            var breadcrumb  = findParents(allCssClassModules, node['id'], true);
+            var breadcrumb  = findParents(allCssClassModules, node.id, true);
             var option = [];
-            option.push(node['id'].replace(/MUNIT/g, 'STS'), breadcrumb.reverse().join(' > '));
+            option.push(node.id.replace(/MUNIT/g, 'STS'), breadcrumb.reverse().join(' > '));
             styleSets.push(option);
         });
         styleSets.sort(sortBreadcrumbs);
@@ -35,14 +35,14 @@ define(['jquery', 'CMS', 'rz_root/notlive/js/cssHelper'], function ($, CMS, cssH
             breadcrumbs = [];
         }
         data.forEach(function (node) {
-            if (parentId == node['id']) {
+            if (parentId == node.id) {
                 var styleSetName = node.formValues.cssStyleSetName.value;
-                if (styleSetName != '') {
+                if (styleSetName !== '') {
                     breadcrumbs.push(styleSetName);
                 }
                 // if a parent exists recursively call this function until no more parent is found
-                if (node['parentUnitId']) {
-                    findParents(data, node['parentUnitId'], false);
+                if (node.parentUnitId) {
+                    findParents(data, node.parentUnitId, false);
                 }
             }
         });
@@ -53,7 +53,7 @@ define(['jquery', 'CMS', 'rz_root/notlive/js/cssHelper'], function ($, CMS, cssH
     var sortBreadcrumbs = function(a,b) {
         a = a[1];
         b = b[1];
-        return a == b ? 0 : (a < b ? -1 : 1)
+        return a == b ? 0 : (a < b ? -1 : 1);
     };
 
     var updateName = function (cfg) {
