76fb1ece81dba88c496be754b3abee022a958660
Sebastian Roller <sebastian.roller@rukzuk.com>
2018-06-19 14:35:07 +0200

    rz_style_iconset: cdn removed, font will be loaded from local server

--- app/sets/rukzuk/rz_core/modules/rz_style_iconset/module/rz_style_iconset.php
+++ app/sets/rukzuk/rz_core/modules/rz_style_iconset/module/rz_style_iconset.php
@@ -3,16 +3,4 @@ namespace Rukzuk\Modules;
 
 class rz_style_iconset extends SimpleModule {
 
-  /**
-   * Output content for HTML head
-   * @param \Render\APIs\APIv1\CSSAPI $api
-   * @param \Render\Unit $unit
-   * @param \Render\ModuleInfo $moduleInfo
-   * @return string
-   */
-  public function htmlHead($api, $moduleInfo)
-  {
-	$html = '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">';
-    return $html;
-  }
 }
