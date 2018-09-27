9abfe059484d6f82d1e6763c049d7c6bdbe96bd8
Karl Glatz <karl@glatz.biz>
2018-09-27 11:15:25 +0200

    sets: fix lodash update (contains is now includes)

--- app/sets/rukzuk/Gruntfile.js
+++ app/sets/rukzuk/Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function (grunt) {
     if (!packageIds && buildChannel) {
         var channelMap = grunt.file.readJSON('channelmap.json');
         var packageIdsInChannel = _.map(_.filter(channelMap.packages, function (obj) {
-            return _.contains(obj.channels, buildChannel);
+            return _.includes(obj.channels, buildChannel);
         }), 'id');
         packageIds = packageIdsInChannel.join(',');
     }
