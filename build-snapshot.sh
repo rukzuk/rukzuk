9ac5cff47cc77e31acef749435d290e10162833f
Karl Glatz <karl@glatz.biz>
2019-07-24 14:14:20 +0200

    build script: first paramter is now used as channel

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -6,19 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 VER=$(cd ${DIR} && git describe --tags --always --dirty)
 BRANCH=$(cd ${DIR} && git rev-parse --abbrev-ref HEAD)
+CHANNEL=${1:-dev}
 
 # clean
 echo "remove ${DIR}/build ${DIR}/packaging ${DIR}/artifacts"
 rm -rf ${DIR}/build ${DIR}/packaging ${DIR}/artifacts
 
-echo "build ${VER} on ${BRANCH}"
+echo "build version ${VER} on branch ${BRANCH} for channel ${CHANNEL}"
 
 # build sets (all modules) in subshell
-echo $(cd ${DIR}/app/sets/rukzuk && grunt build --channel=dev --build="${VER}" --branch="${BRANCH}")
+echo $(cd ${DIR}/app/sets/rukzuk && grunt build --channel=${CHANNEL} --build="${VER}" --branch="${BRANCH}")
 
 # build client
 cd ${DIR}
-grunt package --channel=dev --build="${VER}" --branch="${BRANCH}"
+grunt package --channel=${CHANNEL} --build="${VER}" --branch="${BRANCH}"
 
 # show result
 ls -lh ${DIR}/artifacts/*
