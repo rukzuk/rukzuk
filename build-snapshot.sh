aef271249d22ee3f3b75597e49e67006256c1418
Karl Glatz <karl@glatz.biz>
2019-07-24 11:27:02 +0200

    add current branch to build script

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -5,20 +5,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 VER=$(cd ${DIR} && git describe --tags --always --dirty)
-
+BRANCH=$(cd ${DIR} && git rev-parse --abbrev-ref HEAD)
 
 # clean
 echo "remove ${DIR}/build ${DIR}/packaging ${DIR}/artifacts"
 rm -rf ${DIR}/build ${DIR}/packaging ${DIR}/artifacts
 
-echo "build ${VER}"
+echo "build ${VER} on ${BRANCH}"
 
 # build sets (all modules) in subshell
-echo $(cd ${DIR}/app/sets/rukzuk && grunt build --channel=dev --build=${VER})
+echo $(cd ${DIR}/app/sets/rukzuk && grunt build --channel=dev --build="${VER}" --branch="${BRANCH}")
 
 # build client
 cd ${DIR}
-grunt package --channel=dev --build=${VER}
+grunt package --channel=dev --build="${VER}" --branch="${BRANCH}"
 
 # show result
 ls -lh ${DIR}/artifacts/*
