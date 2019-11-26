aa28a45a89c072a440566316b2f58c458a8befe8
Karl Glatz <karl@glatz.biz>
2019-11-26 19:08:18 +0100

    docker: use proper phantomjs path (with wrapper)

--- docker/config.php
+++ docker/config.php
@@ -5,6 +5,12 @@ return array(
     'dbname' => getenv('CMS_SQLITE_DB'),
     'adapter' => 'pdo_sqlite'
   ),
+  'screens' => array(
+    'type' => 'phantomjs',
+    'phantomjs' => array(
+      'command' => '/usr/local/bin/phantomjs',
+    ),
+  ),
   'webhost' => getenv('CMS_URL') ? : 'http://localhost:8080',
   'internalWebhost' => getenv('CMS_URL_INTERNAL') ? : 'http://localhost',
 );
